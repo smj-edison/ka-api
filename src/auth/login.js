37211d0fa3b42ad63856550c3dfedea372994098
smj-edison <stephen.mason.jones@gmail.com>
2021-02-18 21:35:49 -0800

    Add function to log in and get session cookies

---
+++ src/auth/login.js
@@ -0,0 +1,59 @@
+const axios = require("axios");
+
+const LOGIN_QUERY = require("../queries/loginQuery.js");
+
+const {cookiesToCookieString, getCookieValue, mergeCookies} = require("../cookies/cookies.js");
+const {getSessionCookies} = require("./session.js");
+const generateFKey = require("./generateFKey.js");
+
+/**
+ * Logs into khan academy given a list of current session cookies
+ * 
+ * @param {Array[string]} cookies 
+ * @param {string} username 
+ * @param {string} password 
+ */
+async function loginWithCookies(cookies, username, password) {
+    return axios.post("https://www.khanacademy.org/api/internal/graphql/loginWithPasswordMutation", {
+        "operationName": "loginWithPasswordMutation",
+        "variables": {
+            "identifier": username,
+            "password": password
+        },
+        "query": LOGIN_QUERY
+    }, { 
+        "headers": {
+            "Cookie": cookiesToCookieString(cookies),
+            "x-ka-fkey": getCookieValue(cookies, "fkey")
+        }
+    }).then((result) => {
+        return result.headers["set-cookie"];
+    });
+}
+
+/**
+ * Logs into Khan Academy given a username and password, and returns the session cookies
+ * 
+ * @param {string} username 
+ * @param {string} password 
+ * 
+ * @returns session cookies
+ */
+async function login(username, password) {
+    if(username === undefined || password === undefined) {
+        throw "Username and or password missing!";
+    }
+
+    let sessionCookies = await getSessionCookies();
+
+    sessionCookies.push(`fkey=${generateFKey()}; expires=${(new Date()).toUTCString()}; path=/`);
+
+    let loginCookies = await loginWithCookies(sessionCookies, username, password);
+    let cookies = mergeCookies(sessionCookies, loginCookies);
+
+    return cookies;
+}
+
+module.exports = {
+    login
+};
