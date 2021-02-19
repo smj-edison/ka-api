2f1172b6ed7ce3b8eeaf7c2131d0c1fc3cf0798b
smj-edison <stephen.mason.jones@gmail.com>
2021-02-18 21:20:50 -0800

    Add code for authenticated requests

---
+++ src/request/authenticatedRequest.js
@@ -0,0 +1,83 @@
+const axios = require("axios");
+
+const {cookiesToCookieString, getCookieValue} = require("../cookies/cookies.js");
+
+/**
+ * Make a GET request with the proper authentication on Khan Academy
+ * 
+ * @param {Array} cookies A list of cookies returned from the server
+ * @param {string} url The url on Khan Academy to make the GET request
+ * @param {object} customHeaders Object of custom headers
+ *
+ * @returns {Promise} A promise that resolves to the response
+ */
+async function makeAuthenticatedGetRequest(cookies, url, customHeaders={}) {
+    return axios.get(url, {
+        "headers": {
+            "Cookie": cookiesToCookieString(cookies),
+            "X-KA-FKey": getCookieValue(cookies, "fkey"),
+            ...customHeaders
+        }
+    });
+}
+
+/**
+ * Make a POST request with the proper authentication on Khan Academy
+ * 
+ * @param {Array} cookies A list of cookies returned from the server
+ * @param {string} url The url on Khan Academy to make the POST request
+ * @param {object} body The JSON body of the POST request
+ * @param {object} customHeaders Object of custom headers
+ *
+ * @returns {Promise} A promise that resolves to the response
+ */
+async function makeAuthenticatedPostRequest(cookies, url, body, customHeaders={}) {
+    return axios.post(url, body, {
+        "headers": {
+            "Cookie": cookiesToCookieString(cookies),
+            "X-KA-FKey": getCookieValue(cookies, "fkey"),
+            ...customHeaders
+        }
+    });
+}
+
+/**
+ * Make a PUT request with the proper authentication on Khan Academy
+ * 
+ * @param {Array} cookies A list of cookies returned from the server (set-cookie header)
+ * @param {string} url The url on Khan Academy to make the PUT request
+ * @param {object} body The JSON body of the PUT request
+ * @param {object} customHeaders Object of custom headers
+ *
+ * @returns {Promise} A promise that resolves to the response
+ * 
+ */
+async function makeAuthenticatedPutRequest(cookies, url, body, customHeaders={}) {
+    return axios.put(url, body, {
+        "headers": {
+            "Cookie": cookiesToCookieString(cookies),
+            "X-KA-FKey": getCookieValue(cookies, "fkey"),
+            ...customHeaders
+        }
+    });
+}
+
+/**
+ * Make a DELETE request with the proper authentication on Khan Academy
+ * 
+ * @param {Object} cookies A list of cookies returned from the server
+ * @param {string} url The resource to delete
+ * @param {object} customHeaders Object of custom headers
+ *
+ * @returns {Promise} A promise that resolves to the response
+ * 
+ */
+async function makeAuthenticatedDeleteRequest(cookies, url, customHeaders={}) {
+    return axios.delete(url, {
+        "headers": {
+            "Cookie": cookiesToCookieString(cookies),
+            "X-KA-FKey": getCookieValue(cookies, "fkey"),
+            ...customHeaders
+        }
+    })
+}
