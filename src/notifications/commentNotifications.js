31797b61da20c65671281a79b1dec9892b9f44cb
smj-edison <stephen.mason.jones@gmail.com>
2021-02-19 20:59:31 -0800

    Add function for getting comment notification details

---
+++ src/notifications/commentNotifications.js
@@ -0,0 +1,25 @@
+const {getCommentsOnComment} = require("../discussion/commentsOnComment.js");
+
+/**
+ * Given a comment notification this will return that notification's comment details as well as the thread it's in
+ * 
+ * @param {object} notificationJson - The raw json from the @see getNotificationsRequest result
+ * @param {Array<object>} prerequestedComments - If the posts request was already made,
+ * don't make it again by providing the existing request
+ * 
+ * @returns {object} - A object containing the requested post and the thread it was in
+ */
+async function getNotificationCommentDetails(notificationJson, prerequestedComments) {
+    let {feedbackHierarchy} = notificationJson;
+
+    let comments = prerequestedComments || (await getCommentsOnComment(feedbackHierarchy[feedbackHierarchy.length - 1]));
+
+    return {
+        post: comments.find(comment => comment.expandKey === notification.feedback),
+        posts: comments
+    };
+}
+
+module.exports = {
+    getNotificationCommentDetails
+};
