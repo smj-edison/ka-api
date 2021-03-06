a1fadd660ec432f5de96ecc2d295983e6062399f
smj-edison <stephen.mason.jones@gmail.com>
2021-03-05 22:17:28 -0800

    Update documentation

--- src/notifications/commentNotifications.js
+++ src/notifications/commentNotifications.js
@@ -4,7 +4,7 @@ const {getCommentsOnComment} = require("../discussion/commentsOnComment.js");
  * Given a comment notification this will return that notification's comment details as well as the thread it's in
  * 
  * @param {object} notificationJson - The raw json from the @see getNotificationsRequest result
- * @param {Array<object>} prerequestedComments - If the posts request was already made,
+ * @param {Array<object>} [prerequestedComments] - If the posts request was already made,
  * don't make it again by providing the existing request
  * 
  * @returns {object} - A object containing the requested post and the thread it was in
