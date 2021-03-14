0c587c20f91db09274ba65dd5e8ce5de02eafde3
smj-edison <stephen.mason.jones@gmail.com>
2021-03-13 22:52:39 -0800

    Add function to load spinoffs

---
+++ src/programs/getSpinoffs.js
@@ -0,0 +1,22 @@
+const axios = require("axios");
+
+const SORTING_TYPE = require("./sortingType.js");
+
+async function getSpinoffs(programId, sortingType, limit) {
+    let sortType = 1; // default is most votes
+    const limit = limit || 10000;
+
+    if(sortingType === SORTING_TYPE.MOST_VOTES) {
+        sortType = 1;
+    } else if(sortingType === SORTING_TYPE.NEWEST) {
+        sortType = 2;
+    }
+
+    const url = `https://www.khanacademy.org/api/internal/scratchpads/Scratchpad:${programId}/top-forks?casing=camel&subject=all&sort=${sortType}&page=0&limit=${limit}`;
+
+    return axios.get(url).then(response => response.data);
+}
+
+module.exports = {
+    getSpinoffs
+};
---
+++ src/programs/sortingType.js
@@ -0,0 +1,6 @@
+const SORTING_TYPE = Object.freeze({
+    "MOST_VOTES": "MOST_VOTES",
+    "NEWEST": "NEWEST"
+});
+
+module.exports = SORTING_TYPE;
